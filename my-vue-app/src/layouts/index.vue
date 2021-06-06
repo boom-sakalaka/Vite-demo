6dc5b4f38dc0c870d1a60cadd5f11238d577b62f
Gu <936285702@qq.com>
2021-06-06 10:50:50 +0800

    element3

---
+++ my-vue-app/src/layouts/index.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-06-06 10:42:08
+ * @LastEditTime: 2021-06-06 10:43:36
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vite\my-vue-app\src\layouts\index.vue
+-->
+<template>
+  <div>
+    <!-- 侧边栏 -->
+    <div></div>
+    <!-- 右侧内容区 -->
+    <div>
+      <navbar></navbar>
+      <app-main></app-main>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- my-vue-app/src/plugins/element3.js
+++ my-vue-app/src/plugins/element3.js
@@ -1,23 +1,23 @@
 /*
  * @Author: your name
  * @Date: 2021-06-06 10:35:13
- * @LastEditTime: 2021-06-06 10:40:09
+ * @LastEditTime: 2021-06-06 10:46:14
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vite\my-vue-app\src\plugins\element3.js
  */
-// // 全局引入element3
-// import element3 from 'element3';
-// import 'element3/lib/theme-chalk/index.css';
+// 全局引入element3
+import element3 from 'element3';
+import 'element3/lib/theme-chalk/index.css';
 
 //按需加载
-import { ElButton } from 'element3';
-import 'element3/lib/theme-chalk/button.css';
+// import { ElButton } from 'element3';
+// import 'element3/lib/theme-chalk/button.css';
 
 export default function (app) {
   // 完整
-  //app.use(element3);
+  app.use(element3);
 
   // 按需引入
-  app.use(ElButton);
+  // app.use(ElButton);
 }
