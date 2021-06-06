46ff1de5cca01fbe76faaba76742ddffbf944b84
Gu <936285702@qq.com>
2021-06-06 13:13:19 +0800

    修改触发 git actions

--- my-vue-app/src/views/home.vue
+++ my-vue-app/src/views/home.vue
@@ -1,14 +1,14 @@
 <!--
  * @Author: your name
  * @Date: 2021-06-06 09:41:09
- * @LastEditTime: 2021-06-06 09:44:01
+ * @LastEditTime: 2021-06-06 13:13:00
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vite\my-vue-app\src\views\home.vue
 -->
 <template>
   <h1>home</h1>
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <HelloWorld msg="Hello Vue 3 + Vite + git actions" />
 </template>
 
 <script setup>
