a1552f0cfeae9fd8549a6df4a372f1f4e938c162
gzh <936285702@qq.com>
2021-06-07 13:29:18 +0800

    修改描述

--- my-vue-app/src/views/home.vue
+++ my-vue-app/src/views/home.vue
@@ -1,14 +1,14 @@
 <!--
  * @Author: your name
  * @Date: 2021-06-06 09:41:09
- * @LastEditTime: 2021-06-06 14:22:28
+ * @LastEditTime: 2021-06-07 13:29:09
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vite\my-vue-app\src\views\home.vue
 -->
 <template>
   <h1>home</h1>
-  <HelloWorld msg="Hello Vue 3 + Vite + git actions + tengxunyun" />
+  <HelloWorld msg="Hello Vue 3 + Vite + git actions + 腾讯云" />
 </template>
 
 <script setup>
