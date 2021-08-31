acc245833b1f4d91d51dddcc4e89dcc5b59c8b0a
gzh <936285702@qq.com>
2021-08-31 12:11:43 +0800

    提交iframe 测试

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-06-02 22:46:49
- * @LastEditTime: 2021-08-19 19:24:03
+ * @LastEditTime: 2021-08-31 12:10:37
  * @LastEditors: GZH
  * @Description: In User Settings Edit
  * @FilePath: \Vite-demo\my-vue-app\src\App.vue
@@ -9,13 +9,23 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   <div>
-    <router-link to="/">首页22222</router-link>
+    <router-link to="/">首页1</router-link>
     |
     <router-link to="/lifeCycle">生命周期</router-link>
     |
     <router-link to="/setupProps">setup的参数</router-link>
     |
     <router-link to="/privideInject">Privide/Inject</router-link>
+    <div>
+      <h2>camear</h2>
+      <input type="file" accept="image/*" capture="camera" />
+      <br />
+      <h2>camcorder</h2>
+      <input type="file" accept="video/*" capture="camcorder" />
+      <br />
+      <h2>microphone</h2>
+      <input type="file" accept="audio/*" capture="microphone" />
+    </div>
     <router-view></router-view>
   </div>
 </template>
